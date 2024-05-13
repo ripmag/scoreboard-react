12edec0b69f80f51e65ce0a6673f800ed48acbc5
Eduard <49712904+ripmag@users.noreply.github.com>
2024-05-14 00:34:29 +0500

    Revert "delete All from App"
    This reverts commit 8a6041da3bec654daaf105db0fef68b92a3738f0.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 // import './App.css';
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
-// import Container from '@mui/material/Container';
-// import CircularProgress from '@mui/material/CircularProgress';
+import Container from '@mui/material/Container';
+import CircularProgress from '@mui/material/CircularProgress';
 
-// import GamesList from './components/GamesList';
-// import GameBoard from './components/GameBoard';
+import GamesList from './components/GamesList';
+import GameBoard from './components/GameBoard';
 
 // import { useSelector, useDispatch } from 'react-redux'
 // import { getGamesList } from './features/games/gamesSlice.ts';
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import { Layout } from './components/Layout.js';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Layout } from './components/Layout.js';
 
 function App() {
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
   // // const list = useSelector((state) => state.games.list);
   // const isReady = useSelector((state) => state.games.isReady);
   // const dispatch = useDispatch();
@@ -42,12 +25,12 @@ function App() {
 
 
   // if (!isReady) 
-  //   {
-  //   return (
-  //     <Container maxWidth="sm">
-  //       <CircularProgress />
-  //     </Container>)
-  // }
+    {
+    return (
+      <Container maxWidth="sm">
+        <CircularProgress />
+      </Container>)
+  }
 
   // return (
   //   <>
